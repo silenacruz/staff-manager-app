1ea76d52be712ff44e2ef622857b6bd5d09ec8ea
silenacruz <silene_cruz@hotmail.com>
2026-03-30 15:21:16 +0200

    Security update

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,12 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://hyqtfpuaywuaqodpsloy.supabase.co'
-const supabaseAnonKey = 'sb_publishable_67lk_-y68NzPXzFUusyNrA_G0EjeKDQ'
+// Vite usa 'import.meta.env' para leer el archivo .env
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+// Validamos que existan las variables para evitar errores raros
+if (!supabaseUrl || !supabaseAnonKey) {
+  console.error("⚠️ Supabase credentials are missing in .env file!");
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
